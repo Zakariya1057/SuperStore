b56f920e0137f7dd10ffdd38b3ec675c27053d1e
Zakariya Mohummed <zakaria2011@live.no>
2020-10-04 10:16:43 +0100

    Loading. Filter Search Results

---
+++ SuperStore/Views/RefineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RefineTableViewCell.swift
+//  SuperStore
+//
+//  Created by Zakariya Mohummed on 02/10/2020.
+//  Copyright © 2020 Zakariya Mohummed. All rights reserved.
+//
+
+import UIKit
+
+class RefineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
