b3c8dd67c9a2684eca87122e54d5b3db9a5410b1
Zakariya Mohummed <zakaria2011@live.no>
2021-06-20 21:46:06 +0100

    Moved Nutrition Nib Files Around

---
+++ SuperStore/Scenes/Product/ShowNutrition/Sections/NutrionSectionHeader.swift
@@ -0,0 +1,21 @@
+//
+//  nutrionSectionHeader.swift
+//  SuperStore
+//
+//  Created by Zakariya Mohummed on 20/06/2021.
+//  Copyright © 2021 Zakariya Mohummed. All rights reserved.
+//
+
+import UIKit
+
+class NutrionSectionHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
